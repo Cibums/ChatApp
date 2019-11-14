c1fec18f36c049d9d509420fe99b2384c0acae64
Cibums <56683094+Cibums@users.noreply.github.com>
2019-11-14 22:19:10 +0100

    Commented More

--- SQLRegistration/SQLRegistration/Conversation.cs
+++ SQLRegistration/SQLRegistration/Conversation.cs
@@ -15,10 +15,12 @@ namespace SQLRegistration
 
         public static List<Conversation> GetConversations(int userID)
         {
-            //Creates SQL-query, selectes logged in user's id
+            //Gets all conversation data
             String sql = @"SELECT * FROM `conversations`;";
             Connection.command = new MySqlCommand(sql, Connection.connection);
             Connection.reader = Connection.command.ExecuteReader(); //Executes the query
+
+            //Creates list of Conversation instances 
 
             List<Conversation> conversations = new List<Conversation>();
 
@@ -47,6 +49,7 @@ namespace SQLRegistration
 
                 conv.usersInConversation = users;
 
+                //Check if id list contains the specific user's id, then add it to the list of Conversation instances
                 foreach (int id in conv.usersInConversation)
                 {
                     if (id == userID)
@@ -79,12 +82,14 @@ namespace SQLRegistration
         {
             //Adds user to conversation
 
-            //
+            //Selects data from conversation with ID of the active conversation
             String sql = @"SELECT * FROM `conversations` WHERE `ID`='" + activeConversationID + @"';";
             Connection.command = new MySqlCommand(sql, Connection.connection);
             Connection.reader = Connection.command.ExecuteReader(); //Execute query
             Connection.reader.Read();
 
+            //Updates the users in conversation
+            //(Gets the string of users in the conversations, add the new user and put the new value back in the database)
             string usersString = "";
 
             if (Connection.reader.HasRows)
@@ -104,7 +109,7 @@ namespace SQLRegistration
 
         public static List<int> GetConversationUsers(int conversationID)
         {
-            //Creates SQL-query, selectes logged in user's id
+            //Selects conversation from ID
             String sql = @"SELECT * FROM `conversations` WHERE `ID`='" + conversationID + @"'";
             Connection.command = new MySqlCommand(sql, Connection.connection);
 
@@ -126,6 +131,8 @@ namespace SQLRegistration
 
             //Splits the user's friends' ids into int list
             string[] operands = Regex.Split(Connection.reader[2].ToString(), @"\s+");
+
+            //Gets and returns a list of the IDs of all users in this specific conversation
 
             List<int> returnValue = new List<int>();
 
