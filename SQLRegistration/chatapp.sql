d2efee8a77002093c6f0124a913444e821295ddd
Cibums <56683094+Cibums@users.noreply.github.com>
2019-11-14 20:36:04 +0100

    New Database

--- SQLRegistration/chatapp.sql
+++ SQLRegistration/chatapp.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.7.9
 -- https://www.phpmyadmin.net/
 --
 -- Värd: 127.0.0.1:3306
--- Tid vid skapande: 17 okt 2019 kl 13:02
--- Serverversion: 5.7.26
--- PHP-version: 7.2.18
+-- Tid vid skapande: 14 nov 2019 kl 00:57
+-- Serverversion: 5.7.21
+-- PHP-version: 5.6.35
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -36,7 +36,7 @@ CREATE TABLE IF NOT EXISTS `conversations` (
   `name` varchar(255) NOT NULL,
   `userIDsString` varchar(255) NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -48,10 +48,10 @@ DROP TABLE IF EXISTS `messages`;
 CREATE TABLE IF NOT EXISTS `messages` (
   `ID` int(9) NOT NULL AUTO_INCREMENT,
   `conversationID` int(9) NOT NULL,
-  `messagetext` varchar(255) NOT NULL,
-  `messageaccountID` int(9) NOT NULL,
+  `userID` int(11) NOT NULL,
+  `messagetext` varchar(255) NOT NULL DEFAULT '',
   PRIMARY KEY (`ID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=65 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -69,14 +69,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `lastname` varchar(255) NOT NULL,
   `frienduserIDsString` varchar(255) NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=MyISAM AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
-
---
--- Dumpning av Data i tabell `users`
---
-
-INSERT INTO `users` (`ID`, `username`, `password`, `email`, `firstname`, `lastname`, `frienduserIDsString`) VALUES
-(22, 'cibum', 'hejhej', 'hejhej', 'hejhej', 'hejhej', '');
+) ENGINE=MyISAM AUTO_INCREMENT=35 DEFAULT CHARSET=latin1;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
